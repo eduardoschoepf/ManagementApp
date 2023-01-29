f98bc1d12cb5a814da7c0e0451e44dee14c8b333
eduardo.schoepf <eduardo.schoepf@hotmail.com>
2023-01-29 13:04:26 +0100

    Move firebase folder to src folder

---
+++ src/Firebase/config.js
@@ -0,0 +1,34 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, child, get } from "firebase/database";
+import ENV from "./constants"
+
+const config = {
+  apiKey: ENV.API_KEY,
+  authDomain: ENV.AUTH_DOMAIN,
+  databaseURL: ENV.DATABASE_URL,
+  projectId: ENV.PROJECT_ID,
+  storageBucket: ENV.STORAGE_BUCKET,
+  messagingSenderId: ENV.MESSAGING_SENDER_ID,
+  appId: ENV.APP_ID,
+  measurementId: ENV.MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(config);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
+
+// Test
+const dbRef = ref(db, 'consultants');
+const userId = "karljohnsonexamplecom";
+
+get(child(dbRef, userId)).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+  } else {
+    console.log("There is no data in this node");
+  }
+}).catch((error) => {
+  console.error(error);
+});
